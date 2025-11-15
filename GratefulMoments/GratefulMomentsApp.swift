1730a7b205634c9c214350dd5822a8e5755d2e15
Sergey Petrosyan <ipetriez@icloud.com>
2025-11-15 19:24:21 +0400

    Initial Commit

---
+++ GratefulMoments/GratefulMomentsApp.swift
@@ -0,0 +1,17 @@
+//
+//  GratefulMomentsApp.swift
+//  GratefulMoments
+//
+//  Created by Sergey Petrosyan on 15.11.25.
+//
+
+import SwiftUI
+
+@main
+struct GratefulMomentsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
