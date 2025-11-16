bf7907f455f52159ce4a3d59dee3f15a19715482
Sergey Petrosyan <ipetriez@icloud.com>
2025-11-16 20:00:49 +0400

    Build a data entry view.

---
+++ GratefulMoments/Tabs/Moments/MomentEntryView.swift
@@ -0,0 +1,18 @@
+//
+//  MomentEntryView.swift
+//  GratefulMoments
+//
+//  Created by Sergey Petrosyan on 16.11.25.
+//
+
+import SwiftUI
+
+struct MomentEntryView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MomentEntryView()
+}
