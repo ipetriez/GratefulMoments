05675f9f647707c8743e85dd668519079075cba6
Sergey Petrosyan <ipetriez@icloud.com>
2025-11-18 20:29:51 +0400

    Add presentation button for creating a Grateful Moment.

--- GratefulMoments/ContentView.swift
+++ GratefulMoments/ContentView.swift
@@ -8,12 +8,17 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var presentMomentEntryView = false
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Button("Create a Grateful Moment") {
+                presentMomentEntryView = true
+            }
+            .buttonStyle(.bordered)
+            .sheet(isPresented: $presentMomentEntryView) {
+                MomentEntryView()
+            }
         }
         .padding()
     }
