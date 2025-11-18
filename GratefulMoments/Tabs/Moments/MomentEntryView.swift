850fd65fdc91be2744d222235ae04ae07303c764
Sergey Petrosyan <ipetriez@icloud.com>
2025-11-18 20:15:58 +0400

    Add save and cancel features.

--- GratefulMoments/Tabs/Moments/MomentEntryView.swift
+++ GratefulMoments/Tabs/Moments/MomentEntryView.swift
@@ -7,12 +7,19 @@
 
 import SwiftUI
 import PhotosUI
+import SwiftData
+
 
 struct MomentEntryView: View {
     @State private var title = ""
     @State private var note = ""
     @State private var imageData: Data?
     @State private var newImage: PhotosPickerItem?
+    @State private var isShowingCancelConfirmation = false
+
+
+    @Environment(\.dismiss) private var dismiss
+    @Environment(DataContainer.self) private var dataContainer
     
     var body: some View {
         NavigationStack {
@@ -21,9 +28,46 @@ struct MomentEntryView: View {
             }
             .scrollDismissesKeyboard(.interactively)
             .navigationTitle("Grateful For")
+            .toolbar {
+                ToolbarItem(placement: .cancellationAction) {
+                    Button("Cancel", systemImage: "xmark") {
+                        if title.isEmpty, note.isEmpty, imageData == nil {
+                            dismiss()
+                        } else {
+                            isShowingCancelConfirmation = true
+                        }
+                    }
+                    .confirmationDialog("Discard Moment", isPresented: $isShowingCancelConfirmation) {
+                        Button("Discard Moment", role: .destructive) {
+                            dismiss()
+                        }
+                    }
+                }
+
+
+                ToolbarItem(placement: .confirmationAction) {
+                    Button("Add", systemImage: "checkmark") {
+                        let newMoment = Moment(
+                            title: title,
+                            note: note,
+                            imageData: imageData,
+                            timestamp: .now
+                        )
+                        dataContainer.context.insert(newMoment)
+                        do {
+                            try dataContainer.context.save()
+                            dismiss()
+                        } catch {
+                            // Don't dismiss
+                        }
+                    }
+                    .disabled(title.isEmpty)
+                }
+            }
         }
     }
-    
+
+
     private var photoPicker: some View {
         PhotosPicker(selection: $newImage) {
             Group {
@@ -34,23 +78,22 @@ struct MomentEntryView: View {
                 } else {
                     Image(systemName: "photo.badge.plus.fill")
                         .font(.largeTitle)
-                        .frame (height: 250)
-                        .frame (maxWidth: .infinity)
-                        .background (Color(white: 0.4, opacity: 0.32))
+                        .frame(height: 250)
+                        .frame(maxWidth: .infinity)
+                        .background(Color(white: 0.4, opacity: 0.32))
                 }
-                
             }
-                .clipShape(.rect(cornerRadius: 16))
+            .clipShape(RoundedRectangle(cornerRadius: 16))
         }
         .onChange(of: newImage) {
             guard let newImage else { return }
-            
             Task {
                 imageData = try await newImage.loadTransferable(type: Data.self)
             }
         }
     }
-    
+
+
     var contentStack: some View {
         VStack(alignment: .leading) {
             TextField(text: $title) {
@@ -59,17 +102,21 @@ struct MomentEntryView: View {
             .font(.title.bold())
             .padding(.top, 48)
             Divider()
-            
+
+
             TextField("Log your small wins", text: $note, axis: .vertical)
                 .multilineTextAlignment(.leading)
                 .lineLimit(5...Int.max)
-            
+
+
             photoPicker
         }
         .padding()
     }
 }
 
+
 #Preview {
     MomentEntryView()
+        .sampleDataContainer()
 }
